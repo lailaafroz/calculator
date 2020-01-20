207dbecab2435f4b458cd660b4854f7b7a3c2887
lailaafroz <35794882+lailaafroz@users.noreply.github.com>
2020-01-20 17:48:23 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace myFirstProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myStack<string> ms = new myStack<string>(3);
+            ms.myPush("i");
+            ms.myPush("am");
+            ms.myPush("labonno");
+            ms.myPush("i can");
+            ms.myPush("not");
+            ms.myPush("solve");
+            ms.myPush("solve");
+            ms.myPush("program");
+            Console.WriteLine(ms.myTop());
+        }
+    }
+}
