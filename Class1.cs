708095288ed1f70947868afdc4c88c0c41c13885
lailaafroz <35794882+lailaafroz@users.noreply.github.com>
2020-01-21 11:51:06 +0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculatoR
+{
+    class Class1
+    {
+        public double add() 
+        {
+            Console.Write("laboooo");
+            return 0;
+        }
+    }
+}
